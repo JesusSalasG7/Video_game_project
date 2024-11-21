65b7c5bc32f9067fde846a93b81753fc7e64597b
emilker <125097627+emilker@users.noreply.github.com>
2024-11-21 14:49:51 -0400

    Delete assets/tilemaps/Nueva carpeta directory

--- assets/tilemaps/Nueva carpeta/attante.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.0" name="attante" tilewidth="25" tileheight="17" tilecount="2" columns="2">
- <image source="../../../attante.png" width="50" height="17"/>
-</tileset>
--- assets/tilemaps/Nueva carpeta/dead.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.0" name="dead" tilewidth="32" tileheight="23" tilecount="8" columns="8">
- <image source="../../../dead.png" width="256" height="23"/>
-</tileset>
